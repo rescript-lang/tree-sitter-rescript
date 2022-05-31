4e12c19106c0b7418dbe5474e8aa49e857f6cbbf
Victor Nakoryakov <nail.xx@gmail.com>
2022-05-31 14:56:09 +0300

    fix: functor signatures

--- grammar.js
+++ grammar.js
@@ -160,7 +160,7 @@ module.exports = grammar({
       field('name', $.module_identifier),
       optional(seq(
         ':',
-        field('signature', choice($.block, $.module_expression)),
+        field('signature', choice($.block, $.module_expression, $.functor)),
       )),
       optional(seq(
         '=',
