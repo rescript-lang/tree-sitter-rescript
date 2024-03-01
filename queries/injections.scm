f6ee88fe14fd2f1df15955ea4221f3cbc477143e
Emilio Srougo <emilios1995@gmail.com>
2024-03-01 10:08:11 -0600

    Fix injections

--- queries/injections.scm
+++ queries/injections.scm
@@ -1,21 +1,21 @@
-(comment) @comment
+((comment) @injection.content (#set! injection.language "comment"))
 
 ; %re
 (extension_expression
   (extension_identifier) @_name
   (#eq? @_name "re")
-  (expression_statement (_) @regex))
+  (expression_statement (_) @injection.content (#set! injection.language "regex")))
 
 ; %raw
 (extension_expression
   (extension_identifier) @_name
   (#eq? @_name "raw")
   (expression_statement
-    (_ (_) @javascript)))
+    (_ (_)  @injection.content (#set! injection.language "javascript"))))
 
 ; %graphql
 (extension_expression
   (extension_identifier) @_name
   (#eq? @_name "graphql")
   (expression_statement
-    (_ (_) @graphql)))
+    (_ (_) @injection.content (#set! injection.language "graphql"))))
