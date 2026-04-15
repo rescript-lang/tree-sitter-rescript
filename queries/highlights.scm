f542e541c8ef66d799c36df009dc5ecd0dfd53ad
Manuel Vázquez Acosta <mva.led@gmail.com>
2026-04-15 15:27:45 +0200

    fix: avoid highlighting extension prefixes as operators
    Remove '%' from the unconditional operator query so leading percent signs in
    extension expressions like %foo and %%foo are not highlighted as operators.
    
    Highlight '%' only in binary_expression, matching how other context-sensitive
    operators such as '<', '>', and '/' are handled.

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -224,7 +224,6 @@
   "*"
   "**"
   "*."
-  "%"
   "/."
   "<<"
   ">>"
@@ -253,8 +252,8 @@
 ] @operator
 
 ; Explicitly enclose these operators with binary_expression
-; to avoid confusion with JSX tag delimiters
-(binary_expression ["<" ">" "/"] @operator)
+; to avoid confusion with JSX tag delimiters and extension expressions.
+(binary_expression ["<" ">" "/" "%"] @operator)
 
 [
   "("
